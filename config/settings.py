fc40bccbe7d54f64fdb6cf4579c65cd5b598381f
Ajsteph89 <ajsteph89@gmail.com>
2022-09-13 14:48:53 -0400

    deployed to heroku

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import environ
-
+import django_on_heroku
 
 env = environ.Env(
     # set casting, default value
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -132,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = "core.CustomUser"
+
+django_on_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
